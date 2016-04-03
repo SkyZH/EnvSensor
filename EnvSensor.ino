1ec053933858d8c9788762951d3f78f292fc350d
SkyZH <iSkyZH@gmail.com>
2016-04-03 13:54:44 +0800

    split data structure definition

--- EnvSensor.ino
+++ EnvSensor.ino
@@ -1,44 +1,8 @@
 #include <DHT.h>
 
-#define DHTTYPE DHT22
-#define DHTPIN 7
-
-#define DATA_CMD_TEMPERATURE 97
-#define DATA_CMD_PM 65
-#define DATA_CMD_DEBUG 66
-#define DATA_CMD_FAILED 98
-
-#define PACK_HEADER_LENGTH 2
-#define PACK_DHT_LENGTH 20
-#define PACK_RAW_LENGTH 20
-
-typedef unsigned char CMD;
-typedef unsigned char SIZE;
+#include "data.h"
 
 DHT dht(DHTPIN, DHTTYPE);
-
-struct Header {
-  CMD cmd;
-  SIZE sz;
-} Header;
-
-union HeaderPack {
-  unsigned char pack[PACK_HEADER_LENGTH];
-  struct Header data;
-} HeaderPack;
-
-struct DHTData {
-  float h, t, f, hif, hic;
-} DHTData;
-
-union DHTPack {
-  unsigned char pack[PACK_DHT_LENGTH];
-  struct DHTData data;
-} DHTPack;
-
-struct RawPack {
-  unsigned char pack[PACK_RAW_LENGTH];
-} RawPack;
 
 void setup()
 {
